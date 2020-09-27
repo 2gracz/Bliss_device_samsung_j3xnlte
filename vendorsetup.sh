e688f4f815935c612a40ff52645827226c1fa932
2gracz <andrzej.lenarczyk.5@gmail.com>
2020-09-27 20:50:36 +0200

    blissify

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo dot_j3xnlte-user
-add_lunch_combo dot_j3xnlte-userdebug
-add_lunch_combo dot_j3xnlte-eng
+add_lunch_combo bliss_j3xnlte-user
+add_lunch_combo bliss_j3xnlte-userdebug
+add_lunch_combo bliss_j3xnlte-eng
